5e51d6dbd92ab102647eeda8d942501d880f88df
luca <gianlucatram@gmail.com>
2024-12-17 16:32:17 +0000

    day17 part 2

--- 2024/src/17/17.test.ts
+++ 2024/src/17/17.test.ts
@@ -11,14 +11,13 @@ describe("computer", () => {
   test("parse", () => {
     const computer = new Computer(mockData);
 
-    expect(computer.registers).toEqual({
-      A: 729,
-      B: 0,
-      C: 0,
-    });
+    expect(computer.registers.A.toString()).toEqual("729");
+    expect(computer.registers.B.toString()).toEqual("0");
 
-    expect(computer.instructions).toEqual(
-      expect.arrayContaining([0, 1, 5, 4, 3, 0])
+    expect(computer.registers.C.toString()).toEqual("0");
+
+    expect(computer.instructions.map((i) => i.toString())).toEqual(
+      expect.arrayContaining(["0", "1", "5", "4", "3", "0"])
     );
   });
 
@@ -29,14 +28,14 @@ Register C: 30
 
 Program: 0,1,5,4,3,0`);
 
-    expect(computer.comboOperand(0)).toEqual(0);
-    expect(computer.comboOperand(1)).toEqual(1);
-    expect(computer.comboOperand(2)).toEqual(2);
-    expect(computer.comboOperand(3)).toEqual(3);
-    expect(computer.comboOperand(4)).toEqual(10);
-    expect(computer.comboOperand(5)).toEqual(20);
-    expect(computer.comboOperand(6)).toEqual(30);
-    expect(computer.comboOperand(7)).toEqual(null);
+    expect(computer.comboOperand(0n).toString()).toEqual("0");
+    expect(computer.comboOperand(1n).toString()).toEqual("1");
+    expect(computer.comboOperand(2n).toString()).toEqual("2");
+    expect(computer.comboOperand(3n).toString()).toEqual("3");
+    expect(computer.comboOperand(4n).toString()).toEqual("10");
+    expect(computer.comboOperand(5n).toString()).toEqual("20");
+    expect(computer.comboOperand(6n).toString()).toEqual("30");
+    expect(computer.comboOperand(7n)).toEqual(null);
   });
 
   test("adv", () => {
@@ -46,12 +45,12 @@ Register C: 30
 
 Program: 0,1,5,4,3,0`);
 
-    expect(computer.registers.A).toEqual(16);
-    expect(computer.pointer).toEqual(0);
-    computer.adv({ opcode: 0, operand: 5 });
-    expect(computer.registers.A).toEqual(4);
+    expect(computer.registers.A.toString()).toEqual("16");
+    expect(computer.pointer.toString()).toEqual("0");
+    computer.adv({ opcode: 0n, operand: 5n });
+    expect(computer.registers.A.toString()).toEqual("4");
 
-    expect(computer.pointer).toEqual(2);
+    expect(computer.pointer.toString()).toEqual("2");
   });
 
   test("bxl", () => {
@@ -60,12 +59,12 @@ Register B: 5
 Register C: 30
 
 Program: 0,1,5,4,3,0`);
-    expect(computer.pointer).toEqual(0);
+    expect(computer.pointer.toString()).toEqual("0");
 
-    expect(computer.registers.B).toEqual(5);
-    computer.bxl({ opcode: 0, operand: 3 });
-    expect(computer.registers.B).toEqual(6);
-    expect(computer.pointer).toEqual(2);
+    expect(computer.registers.B.toString()).toEqual("5");
+    computer.bxl({ opcode: 0n, operand: 3n });
+    expect(computer.registers.B.toString()).toEqual("6");
+    expect(computer.pointer.toString()).toEqual("2");
   });
 
   test("bst", () => {
@@ -74,12 +73,12 @@ Register B: 5
 Register C: 30
 
 Program: 0,1,5,4,3,0`);
-    expect(computer.pointer).toEqual(0);
+    expect(computer.pointer.toString()).toEqual("0");
 
-    expect(computer.registers.B).toEqual(5);
-    computer.bst({ opcode: 0, operand: 3 });
-    expect(computer.registers.B).toEqual(3);
-    expect(computer.pointer).toEqual(2);
+    expect(computer.registers.B.toString()).toEqual("5");
+    computer.bst({ opcode: 0n, operand: 3n });
+    expect(computer.registers.B.toString()).toEqual("3");
+    expect(computer.pointer.toString()).toEqual("2");
   });
 
   test("jnz A=0", () => {
@@ -89,11 +88,11 @@ Register C: 30
 
 Program: 0,1,5,4,3,0`);
 
-    expect(computer1.pointer).toEqual(0);
-    expect(computer1.registers.A).toEqual(0);
-    computer1.jnz({ opcode: 0, operand: 3 });
-    expect(computer1.registers.A).toEqual(0);
-    expect(computer1.pointer).toEqual(2);
+    expect(computer1.pointer.toString()).toEqual("0");
+    expect(computer1.registers.A.toString()).toEqual("0");
+    computer1.jnz({ opcode: 0n, operand: 3n });
+    expect(computer1.registers.A.toString()).toEqual("0");
+    expect(computer1.pointer.toString()).toEqual("2");
   });
 
   test("jnz A !== 0", () => {
@@ -103,11 +102,11 @@ Register C: 30
 
 Program: 0,1,5,4,3,0`);
 
-    expect(computer1.pointer).toEqual(0);
-    expect(computer1.registers.A).toEqual(5);
-    computer1.jnz({ opcode: 0, operand: 3 });
-    expect(computer1.registers.A).toEqual(5);
-    expect(computer1.pointer).toEqual(3);
+    expect(computer1.pointer.toString()).toEqual("0");
+    expect(computer1.registers.A.toString()).toEqual("5");
+    computer1.jnz({ opcode: 0n, operand: 3n });
+    expect(computer1.registers.A.toString()).toEqual("5");
+    expect(computer1.pointer.toString()).toEqual("3");
   });
 
   test("bxc", () => {
@@ -116,12 +115,12 @@ Register B: 5
 Register C: 30
 
 Program: 0,1,5,4,3,0`);
-    expect(computer.pointer).toEqual(0);
+    expect(computer.pointer.toString()).toEqual("0");
 
-    expect(computer.registers.B).toEqual(5);
-    computer.bxc({ opcode: 0, operand: 3 });
-    expect(computer.registers.B).toEqual(27);
-    expect(computer.pointer).toEqual(2);
+    expect(computer.registers.B.toString()).toEqual("5");
+    computer.bxc({ opcode: 0n, operand: 3n });
+    expect(computer.registers.B.toString()).toEqual("27");
+    expect(computer.pointer.toString()).toEqual("2");
   });
 
   test("out", () => {
@@ -130,11 +129,11 @@ Register B: 10
 Register C: 30
 
 Program: 0,1,5,4,3,0`);
-    expect(computer.pointer).toEqual(0);
+    expect(computer.pointer.toString()).toEqual("0");
 
-    computer.out({ opcode: 0, operand: 3 });
+    computer.out({ opcode: 0n, operand: 3n });
     expect(computer.outputs).toEqual(["3"]);
-    expect(computer.pointer).toEqual(2);
+    expect(computer.pointer.toString()).toEqual("2");
   });
 
   test("bdv", () => {
@@ -144,12 +143,12 @@ Register C: 30
 
 Program: 0,1,5,4,3,0`);
 
-    expect(computer.registers.A).toEqual(16);
-    expect(computer.pointer).toEqual(0);
-    computer.bdv({ opcode: 0, operand: 5 });
-    expect(computer.registers.B).toEqual(4);
+    expect(computer.registers.A.toString()).toEqual("16");
+    expect(computer.pointer.toString()).toEqual("0");
+    computer.bdv({ opcode: 0n, operand: 5n });
+    expect(computer.registers.B.toString()).toEqual("4");
 
-    expect(computer.pointer).toEqual(2);
+    expect(computer.pointer.toString()).toEqual("2");
   });
 
   test("cdv", () => {
@@ -159,27 +158,16 @@ Register C: 30
 
 Program: 0,1,5,4,3,0`);
 
-    expect(computer.registers.A).toEqual(16);
-    expect(computer.pointer).toEqual(0);
-    computer.cdv({ opcode: 0, operand: 5 });
-    expect(computer.registers.C).toEqual(4);
+    expect(computer.registers.A.toString()).toEqual("16");
+    expect(computer.pointer.toString()).toEqual("0");
+    computer.cdv({ opcode: 0n, operand: 5n });
 
-    expect(computer.pointer).toEqual(2);
+    expect(computer.registers.C.toString()).toEqual("4");
+
+    expect(computer.pointer.toString()).toEqual("2");
   });
 
   describe("run", () => {
-    test("If register C contains 9, the program 2,6 would set register B to 1", () => {
-      const computer = new Computer(`Register A: 16
-Register B: 2
-Register C: 9
-
-Program: 2,6`);
-
-      computer.run();
-
-      expect(computer.registers.B).toEqual(1);
-    });
-
     test("If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.", () => {
       const computer = new Computer(`Register A: 10
 Register B: 2
@@ -200,14 +188,14 @@ Program: 0,1,5,4,3,0`);
 
       computer.run();
       expect(computer.outputs.join(",")).toEqual("4,2,5,6,7,7,7,7,3,1,0");
-      expect(computer.registers.A).toEqual(0);
+      expect(computer.registers.A.toString()).toEqual("0");
     });
 
     test("mockData", () => {
       const computer = new Computer(mockData);
       computer.run();
 
-      expect(computer.outputs).toEqual(
+      expect(computer.outputs.map(String)).toEqual(
         [4, 6, 3, 5, 6, 3, 5, 2, 1, 0].map(String)
       );
     });
@@ -229,28 +217,26 @@ Register C: 0
 Program: 0,3,5,4,3,0`);
 
       computer.run();
-      expect(computer.registers.A).toEqual(0);
+      expect(computer.registers.A.toString()).toEqual("0");
       expect(computer.outputs.join(",")).toEqual("0,3,5,4,3,0");
       expect(computer.debugger.length).toBeGreaterThan(0);
 
-      computer.reverseReset(10);
+      computer.reverseReset(10n);
 
-      expect(computer.registers.A).toEqual(10);
+      expect(computer.registers.A.toString()).toEqual("10");
       expect(computer.outputs).toHaveLength(0);
       expect(computer.debugger).toHaveLength(0);
     });
 
-    test("mock data 2", () => {
-      const computer = new Computer(`Register A: 117440
+    test("real data", () => {
+      const computer = new Computer(`Register A: 60589763
 Register B: 0
 Register C: 0
 
-Program: 0,3,5,4,3,0`);
-      computer.reverse();
+Program: 2,4,1,5,7,5,1,6,4,1,5,5,0,3,3,0`);
+      const res = computer.reverse();
 
-      console.log(computer.debugger);
-
-      expect(computer.outputs.join(",")).toEqual("0,3,5,4,3,0");
+      expect(res).toEqual(107413700225434);
     });
   });
 });
--- 2024/src/17/index.ts
+++ 2024/src/17/index.ts
@@ -1,52 +1,52 @@
-const xor = (a: number, b: number) => Number(BigInt(a) ^ BigInt(b));
-type Instruction = { opcode: number; operand: number };
-type Register = { A: number; B: number; C: number };
+const xor = (a: bigint, b: bigint) => BigInt(a) ^ BigInt(b);
+type Instruction = { opcode: bigint; operand: bigint };
+type Register = { A: bigint; B: bigint; C: bigint };
 export class Computer {
-  instructions: number[];
+  instructions: bigint[];
   registers: Register;
   ogRegisters: Register;
 
-  pointer: number;
+  pointer: bigint;
   outputs: string[];
   debugger: string[];
   constructor(text: string) {
     const [rs, i] = text.split("\n\n");
 
-    const instructions = i.replace("Program: ", "").split(",").map(Number);
+    const instructions = i.replace("Program: ", "").split(",").map(BigInt);
     this.instructions = instructions;
 
     const registers = rs.split("\n").reduce(
-      (acc: { A: number; B: number; C: number }, item) => {
+      (acc: { A: bigint; B: bigint; C: bigint }, item) => {
         const [register, value] = item.replace("Register ", "").split(": ");
 
-        if (register === "A") return { ...acc, A: Number(value) };
-        if (register === "B") return { ...acc, B: Number(value) };
-        if (register === "C") return { ...acc, C: Number(value) };
+        if (register === "A") return { ...acc, A: BigInt(value) };
+        if (register === "B") return { ...acc, B: BigInt(value) };
+        if (register === "C") return { ...acc, C: BigInt(value) };
 
         return acc;
       },
-      { A: 0, B: 0, C: 0 }
+      { A: 0n, B: 0n, C: 0n }
     );
 
     this.registers = registers;
     this.ogRegisters = registers;
-    this.pointer = 0;
+    this.pointer = 0n;
     this.outputs = [];
     this.debugger = [];
   }
 
-  comboOperand(operand: number) {
-    if (operand <= 3) return operand;
+  comboOperand(operand: bigint) {
+    if (operand <= 3n) return operand;
 
-    if (operand === 4) return this.registers.A;
-    if (operand === 5) return this.registers.B;
-    if (operand === 6) return this.registers.C;
+    if (operand === 4n) return this.registers.A;
+    if (operand === 5n) return this.registers.B;
+    if (operand === 6n) return this.registers.C;
 
     return null;
   }
 
   increasePointer(
-    value: number = 2,
+    value: bigint = 2n,
     instruction: string,
     { opcode, operand }: Instruction
   ) {
@@ -54,21 +54,23 @@ export class Computer {
     this.debugger.push(
       JSON.stringify({
         instruction,
-        pointer: this.pointer,
-        register: this.registers,
-        opcode,
-        operand,
+        pointer: this.pointer.toString(),
+        register: Object.entries(this.registers).reduce((acc, [key, value]) => {
+          return { ...acc, [key]: value.toString() };
+        }, {}),
+        opcode: opcode.toString(),
+        operand: operand.toString(),
       })
     );
   }
   adv({ opcode, operand }: Instruction) {
     const power = this.comboOperand(operand);
-    const divisor = Math.pow(2, power);
+    const divisor = BigInt(Math.pow(2, Number(power)));
 
-    const value = Math.trunc(this.registers.A / divisor);
+    const value = BigInt(Math.trunc(Number(this.registers.A / divisor)));
 
     this.registers.A = value;
-    this.increasePointer(2, "adv", { opcode, operand });
+    this.increasePointer(2n, "adv", { opcode, operand });
 
     // this.debugger.push(JSON.stringify({op: 'adv', opcode, operand}))
   }
@@ -77,24 +79,24 @@ export class Computer {
     const b = this.registers.B;
 
     this.registers.B = xor(b, operand);
-    this.increasePointer(2, "bxl", { opcode, operand });
+    this.increasePointer(2n, "bxl", { opcode, operand });
   }
 
   bst({ operand, opcode }: Instruction) {
     const combo = this.comboOperand(operand);
-    const value = combo % 8;
+    const value = combo % 8n;
     this.registers.B = value;
-    this.increasePointer(2, "bst", { opcode, operand });
+    this.increasePointer(2n, "bst", { opcode, operand });
   }
 
   jnz({ opcode, operand }: Instruction) {
-    if (this.registers.A === 0) {
-      this.increasePointer(2, "jnz", { opcode, operand });
+    if (this.registers.A === 0n) {
+      this.increasePointer(2n, "jnz", { opcode, operand });
       return;
     }
 
     this.pointer = operand;
-    this.increasePointer(0, "jnz", { opcode, operand });
+    this.increasePointer(0n, "jnz", { opcode, operand });
   }
 
   bxc({ operand, opcode }: Instruction) {
@@ -102,72 +104,92 @@ export class Computer {
     const c = this.registers.C;
 
     this.registers.B = xor(b, c);
-    this.increasePointer(2, "bxc", { opcode, operand });
+    this.increasePointer(2n, "bxc", { opcode, operand });
   }
 
   out({ operand, opcode }: Instruction) {
     const combo = this.comboOperand(operand);
-    const value = combo % 8;
+    const value = combo % 8n;
 
     this.outputs.push(...String(value).split(""));
-    this.increasePointer(2, "out", { opcode, operand });
+    this.increasePointer(2n, "out", { opcode, operand });
   }
 
   bdv({ operand, opcode }: Instruction) {
     const power = this.comboOperand(operand);
-    const divisor = Math.pow(2, power);
+    const divisor = BigInt(Math.pow(2, Number(power)));
 
-    const value = Math.trunc(this.registers.A / divisor);
+    const value = BigInt(Math.trunc(Number(this.registers.A / divisor)));
 
     this.registers.B = value;
-    this.increasePointer(2, "bdv", { opcode, operand });
+    this.increasePointer(2n, "bdv", { opcode, operand });
   }
 
   cdv({ operand, opcode }: Instruction) {
     const power = this.comboOperand(operand);
-    const divisor = Math.pow(2, power);
+    const divisor = BigInt(Math.pow(2, Number(power)));
 
-    const value = Math.trunc(this.registers.A / divisor);
+    const value = BigInt(Math.trunc(Number(this.registers.A / divisor)));
 
     this.registers.C = value;
-    this.increasePointer(2, "cdv", { opcode, operand });
+    this.increasePointer(2n, "cdv", { opcode, operand });
   }
 
   instruction({ opcode, operand }: Instruction) {
-    if (opcode === 0) return this.adv({ opcode, operand });
-    if (opcode === 1) return this.bxl({ opcode, operand });
-    if (opcode === 2) return this.bst({ opcode, operand });
-    if (opcode === 3) return this.jnz({ opcode, operand });
-    if (opcode === 4) return this.bxc({ opcode, operand });
-    if (opcode === 5) return this.out({ opcode, operand });
-    if (opcode === 6) return this.bdv({ opcode, operand });
-    if (opcode === 7) return this.cdv({ opcode, operand });
+    if (opcode === 0n) return this.adv({ opcode, operand });
+    if (opcode === 1n) return this.bxl({ opcode, operand });
+    if (opcode === 2n) return this.bst({ opcode, operand });
+    if (opcode === 3n) return this.jnz({ opcode, operand });
+    if (opcode === 4n) return this.bxc({ opcode, operand });
+    if (opcode === 5n) return this.out({ opcode, operand });
+    if (opcode === 6n) return this.bdv({ opcode, operand });
+    if (opcode === 7n) return this.cdv({ opcode, operand });
   }
 
   run() {
     while (this.pointer <= this.instructions.length - 1) {
-      const opcode = this.instructions[this.pointer];
-      const operand = this.instructions[this.pointer + 1];
+      const opcode = this.instructions[Number(this.pointer)];
+      const operand = this.instructions[Number(this.pointer) + 1];
 
       this.instruction({ opcode, operand });
     }
   }
 
-  reverseReset(aValue: number) {
+  reverseReset(aValue: bigint) {
     this.debugger = [];
     this.outputs = [];
 
+    this.pointer = 0n;
     this.registers = {
       ...this.ogRegisters,
       A: aValue,
     };
   }
-  reverse() {
-    while (this.pointer <= this.instructions.length - 1) {
-      const opcode = this.instructions[this.pointer];
-      const operand = this.instructions[this.pointer + 1];
 
-      this.instruction({ opcode, operand });
+  reverse() {
+    const expected = this.instructions.join(",");
+
+    this.reverseReset(0n);
+
+    let min = 0n;
+    let found = false;
+
+    while (!found) {
+      this.run();
+      const output = this.outputs.join(",");
+      if (output === expected) {
+        found = true;
+        break;
+      }
+
+      if (expected.endsWith(output)) {
+        min = min * 8n;
+      } else {
+        min++;
+      }
+      this.reverseReset(min);
     }
+
+    return Number(min);
   }
 }
