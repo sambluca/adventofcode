5a5b988ef5da864a8b2296c8f5dee19a51d3ed93
luca <gianlucatram@gmail.com>
2024-12-20 12:29:24 +0000

    copy util

--- 2024/src/utils/index.ts
+++ 2024/src/utils/index.ts
@@ -1,2 +1,8 @@
 export * from "./Number";
 export * from "./Array";
+
+export const copy = (data: any) => {
+  var proc = require("child_process").spawn("pbcopy");
+  proc.stdin.write(JSON.stringify(data));
+  proc.stdin.end();
+};
